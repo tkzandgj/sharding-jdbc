16aafe5745484ebe389dadefe561960525a02162
tukangzheng <tukangzheng@163.com>
2017-08-22 23:23:07 +0800

    add commit

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/AbstractParser.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/AbstractParser.java
@@ -69,17 +69,23 @@ public abstract class AbstractParser {
                 if (equalAny(Symbol.QUESTION)) {
                     increaseParametersIndex();
                 }
+                //到达结尾，或者匹配合适数的)右括号
                 if (Assist.END == getLexer().getCurrentToken().getType() || (Symbol.RIGHT_PAREN == getLexer().getCurrentToken().getType() && 0 == count)) {
                     break;
                 }
+
+                // 处理里面有多个括号的情况，例如：SELECT COUNT(DISTINCT(order_id) FROM t_order
                 if (Symbol.LEFT_PAREN == getLexer().getCurrentToken().getType()) {
                     count++;
                 } else if (Symbol.RIGHT_PAREN == getLexer().getCurrentToken().getType()) {
                     count--;
                 }
+                // 下一个词法
                 getLexer().nextToken();
             }
+            // 获得括号内的内容
             result.append(getLexer().getInput().substring(beginPosition, getLexer().getCurrentToken().getEndPosition()));
+            // 下一个词法
             getLexer().nextToken();
         }
         return result.toString();
